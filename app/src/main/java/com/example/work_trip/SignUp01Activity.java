0b43703f99fa432e87f6c36a270a4e7ed0498c63
[yerim123456] <[yerim1226@gmail.com]>
2023-05-23 04:26:12 +0900

    [xml]sign_up_one

---
+++ app/src/main/java/com/example/work_trip/SignUp01Activity.java
@@ -0,0 +1,14 @@
+package com.example.work_trip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up01);
+    }
+}
