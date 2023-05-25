5c1d6ed63cfc664bb6309979483d27f403d487c9
[yerim123456] <[yerim1226@gmail.com]>
2023-05-25 10:43:08 +0900

    [xml]sign_up_part

---
+++ app/src/main/java/com/example/work_trip/SignUp02Activity.java
@@ -0,0 +1,14 @@
+package com.example.work_trip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up02);
+    }
+}
