3e31446c87be6e574088182aa9a62778df5abff3
[yerim123456] <[yerim1226@gmail.com]>
2023-06-06 16:15:29 +0900

    [java&xml]sign_up_part

--- app/src/main/java/com/example/work_trip/SignUp02Activity.java
+++ app/src/main/java/com/example/work_trip/SignUp02Activity.java
@@ -1,14 +1,103 @@
 package com.example.work_trip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import com.example.work_trip.databinding.ActivitySignUp02Binding;
 
-public class SignUp02Activity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class SignUp02Activity extends AppCompatActivity  implements View.OnClickListener {
+
+    private ActivitySignUp02Binding binding;
+    String id;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up02);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up02);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        password = intent.getStringExtra("password");
+
+
+        binding.etEmail.addTextChangedListener(textWatcher);
+
+        binding.ibEmailCheck.setOnClickListener(this);
+        binding.ibNext.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ib_next:
+                if(!(binding.etName.getText().toString().equals("")) && !(binding.etBirth.getText().toString().equals("")) && !(binding.etEmail.getText().toString().equals(""))){
+
+                    Log.d("Yerim", binding.etName.getText().toString());
+                    Log.d("Yerim", binding.etBirth.getText().toString());
+
+                    Intent it = new Intent(this, SignUp03Activity.class);
+                    it.putExtra("id", id);
+                    it.putExtra("password",password);
+                    it.putExtra("name", binding.etName.getText().toString());
+                    it.putExtra("birth", binding.etBirth.getText().toString());
+                    it.putExtra("email", binding.etEmail.getText().toString());
+
+                    startActivity(it);
+                    finish();
+                }
+                else{
+                    Toast.makeText(this, "채워주세요",Toast.LENGTH_LONG);
+                }
+                break;
+            case R.id.ib_email_check:
+                break;
+
+
+        }
+    }
+
+    public String checkEmailForm(String inputText){
+
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        Matcher matcher = pattern.matcher(inputText);
+
+        if(matcher.find()){
+            //이메일 형식에 맞을 때
+            return "올바른 이메일 입니다.";
+
+        }else{
+            //이메일 형식에 맞지 않을 때
+            return "올바른 이메일을 입력해주세요.";
+
+        }
+    }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // 입력하기 전에 조치
+            binding.tvEmailError.setVisibility(View.VISIBLE);
+            binding.tvEmailError.setText(checkEmailForm(binding.etEmail.getText().toString()));
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // 입력난에 변화가 있을 시 조치
+        }
+
+        public void afterTextChanged(Editable s) {
+            // 입력이 끝났을 때 조치
+        }
+    };
 }
--- app/src/main/java/com/example/work_trip/SignUp03Activity.java
+++ app/src/main/java/com/example/work_trip/SignUp03Activity.java
@@ -1,14 +1,83 @@
 package com.example.work_trip;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class SignUp03Activity extends AppCompatActivity {
+import com.example.work_trip.databinding.ActivitySignUp02Binding;
+import com.example.work_trip.databinding.ActivitySignUp03Binding;
 
+public class SignUp03Activity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    String id;
+    String password;
+    String name;
+
+    String birth;
+    String email;
+
+    private ActivitySignUp03Binding binding;
+    String check_thema;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up03);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up03);
+
+        DBHelper helper;
+        SQLiteDatabase db;
+        helper = new DBHelper(this, "member.db", null, 1);
+        db = helper.getWritableDatabase();
+        helper.onCreate(db);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        password = intent.getStringExtra("id");
+        name = intent.getStringExtra("name");
+        birth = intent.getStringExtra("birth");
+        email = intent.getStringExtra("email");
+
+        binding.thema.setOnCheckedChangeListener(this);
+
+        binding.ibNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!(check_thema.equals(""))){
+                    // sql 처리
+                    String sql = "INSERT INTO mytable('txt') values('Bulgogi');";
+                    db.execSQL(sql);
+
+                    Intent it = new Intent(getApplicationContext(), SignUp04Activity.class);
+                    it.putExtra("name", name);
+                    startActivity(it);
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "선택해주세요",Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+        switch (i){
+            case  R.id.thema1:
+                check_thema = binding.thema1.getText().toString();
+            case  R.id.thema2:
+                check_thema = binding.thema2.getText().toString();
+            case  R.id.thema3:
+                check_thema = binding.thema3.getText().toString();
+            case  R.id.thema4:
+                check_thema = binding.thema4.getText().toString();
+            case  R.id.thema5:
+                check_thema = binding.thema5.getText().toString();
+        }
     }
 }
